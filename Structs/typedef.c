fbd4cb11defb05724447ec359831b865239ab63f
João Pedro Gomes <120976843+JPedroGomes@users.noreply.github.com>
2026-06-07 22:32:06 -0300

    Add files via upload

---
+++ Structs/typedef.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef float nota;
+
+
+int main(){
+
+nota prova1 = 1.0;
+nota prova2 = 8.5;
+
+nota soma = prova1 + prova2;
+
+printf("a soma das notas é: %.1f", soma);
+
+
+return 0;
+}
