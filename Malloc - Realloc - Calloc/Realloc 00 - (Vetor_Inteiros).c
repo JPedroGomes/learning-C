2d79737fcd4f7e30e3ffabbb01c797b53e90cebb
João Pedro Gomes <120976843+JPedroGomes@users.noreply.github.com>
2026-07-27 23:25:50 -0300

    Update Realloc 00 - (Vetor_Inteiros).c

--- Malloc - Realloc - Calloc/Realloc 00 - (Vetor_Inteiros).c
+++ Malloc - Realloc - Calloc/Realloc 00 - (Vetor_Inteiros).c
@@ -1,49 +1,62 @@
 /*
-Faça um programa aloque inicialmente um vetor com 3 inteiros, 
-pergunte ao usuário quantos valores ele
-quer informar, use REALLOC caso seja mais que 3 valores, leia e apresente os valores. 
+==================================================
+Author: João Pedro Gomes
+
+riginal Exercise:
+Create a program that initially allocates an array
+of 3 integers. Ask the user how many values they
+want to enter. Use realloc() if the number of
+values is greater than 3. Read and display all
+entered values.
+
+Enunciado Original:
+Faça um programa aloque inicialmente um vetor com
+3 inteiros, pergunte ao usuário quantos valores ele
+quer informar, use REALLOC caso seja mais que
+3 valores, leia e apresente os valores.
+
+==================================================
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
     int *numeros;
-    
-    numeros = (int*) malloc(3 * sizeof(int));
-    if(numeros == NULL){
-        printf("sem espaço de memoria");
-        free(numeros);
-        return 1;
-    }
-    printf("Informe 3 números:\n");
+    int quantidade;
 
-    for (int i = 0; i < 3; i++) {
-    printf("Número %d: ", i + 1);
-    scanf("%d", &numeros[i]);
-    }
-    
-    printf("Adicionando mais numeros: \n");
-    
-    numeros = (int*) realloc(numeros, 3 * sizeof(int));
-     if(numeros == NULL){
-        printf("sem espaço de memoria");
-        free(numeros);
+    numeros = (int*)malloc(3 * sizeof(int));
+
+    if (numeros == NULL) {
+        printf("Erro ao alocar memoria.\n");
         return 1;
     }
-    
-    printf("digite três numeros: \n");
-    
-    for(int i = 0; i < 3; i++){
-        printf("digite o %d. numero:\n", i + 1);
-        scanf("%d", &numeros[i+3]);
+
+    printf("Quantos numeros deseja informar? ");
+    scanf("%d", &quantidade);
+
+    if (quantidade > 3){
+        
+        numeros = (int*)realloc(numeros, quantidade * sizeof(int));
+        if (numeros == NULL) {
+            printf("Erro ao realocar memoria.\n");
+            return 1;
+        }
     }
     
-    printf("Os numeros digitados são: \n");
-    
-   for(int i = 0; i < 6; i++){
-        printf("numero %d. >> %d:\n", i + 1, numeros[i]);
-        
+    printf("\n -==========================================================-\n");
+    printf("Digite os %d numeros:\n", quantidade);
+
+    for(int i = 0; i < quantidade; i++){
+        printf("Numero %d: ", i + 1);
+        scanf("%d", &numeros[i]);
+    }
+
+    printf("\nNumeros informados:\n");
+
+    for (int i = 0; i < quantidade; i++) {
+        printf("%d\n", numeros[i]);
     }
 
     free(numeros);
